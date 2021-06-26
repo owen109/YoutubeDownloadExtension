51fea39bc3a2b152120aad020a7fb9b1875d5b54
owen109 <owen.p.dorsey@gmail.com>
2021-06-26 16:06:22 -0400

    first commit

---
+++ Exstension/script.js
@@ -0,0 +1,12 @@
+var convertBtn = document.querySelector('.convert-button');
+
+convertBtn.addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+        let url = tabs[0].url;
+        let format = document.querySelector('input[name="format"]:checked').id;
+        sendURL(url,format)
+    })
+});
+function sendURL(URL, FORMAT) {
+    window.location.href = `http://localhost:3000/download?URL=${URL}&FORMAT=${FORMAT}`;
+}
---
+++ Server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+const fs = require('fs')
+const path = require('path')
+
+app.use(cors());
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server Works !!! At port ${PORT}`);
+})
+
+app.get('/download', async function(req,res){
+    var URL = req.query.URL;
+    var FORMAT = req.query.FORMAT;
+    console.log(FORMAT);
+    res.header('Content-Disposition', `attachment; filename="untitled.${FORMAT}"`);
+    ytdl(URL,  { filter: (FORMAT == 'mp4' ? format => format.container === 'mp4' : 'audioonly') } ).pipe(res)
+});
